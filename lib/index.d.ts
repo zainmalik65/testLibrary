73768b8331bbb69c3bd63fe682ae47412d7a13c9
zain <zainmalik9964@gmail.com>
2021-09-24 10:13:21 +0500

    lib is ready to publish

---
+++ lib/index.d.ts
@@ -0,0 +1,1 @@
+export declare function hello_world(a1: string, a2: number): void;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function hello_world(a1: string, a2: number){
+    console.log(`Hello World! I was given the string ${a1} and the number ${a2}`)
+}
